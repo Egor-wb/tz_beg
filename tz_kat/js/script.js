20dd8997cac35506dc19e242b7fe9e8afe6c4b00
Egor-wb <75799273+Egor-wb@users.noreply.github.com>
2022-06-09 13:54:30 +0300

    Add files via upload

---
+++ tz_kat/js/script.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+	$('.menu-burger__header').click(function(){
+        $('.menu-burger__header').toggleClass('open-menu');
+        $('.header__nav').toggleClass('open-menu');
+        $('body').toggleClass('fixed-page');
+	});
+});
+
+
+
+$(function() {
+    $('.news_block_item_slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        speed: 1500,
+        responsive: [
+        {
+                breakpoint: 980,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    arrows: false,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false,
+                    infinite: true,
+                }
+            }
+        ]
+       
+    });
+});
+
